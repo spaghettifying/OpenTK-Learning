ba72d579ebd805504906953fdbdc92bf40e719f2
spaghettifying <spaghettifying@users.noreply.github.com>
2023-03-21 02:02:09 +0000

    Learnt about Vertex Input and NDC
    Vertex Input: OpenGL is 3D graphics library so coordinates have to be 3D
    These coords have to be between -1.0 and 1.0, these are called Normalized Device Coordinates, if not in range they will be discarded and not visible.

--- Hello Triangle/Program.cs
+++ Hello Triangle/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
 
-namespace Hello_Triangle;
-
-class Program
-{
-    static void Main()
-    {
-        //Continue the tutorial at https://opentk.net/learn/chapter1/2-hello-triangle.html#vertex-input
-    }
-}
+Console.WriteLine("Hello World");
+Console.ReadLine();
--- Hello Triangle/Window.cs
+++ Hello Triangle/Window.cs
@@ -6,8 +6,36 @@ using ClearBufferMask = OpenTK.Graphics.OpenGLES1.ClearBufferMask;
 
 namespace Hello_Triangle;
 
+/*  Vertex Input:
+ *  OpenGL is a 3D graphics library, and as such uses 3D coordinates
+ *  It doesn't simply transform 3D coordinates to 2D pixels on the screen; OpenGL only processes 3D coordinates when they're in a specific range between -1.0 and 1.0
+ *  All coordinates within this so called normalized device coordinates range will end up visible on the screen
+ *
+ *  For this program, we are trying to render a single triangle
+ *  We want to specify a total of three vertices with each vertex having a 3D position
+ *  We define them in normalized device coordinates in a float array
+ *  Because OpenGL works in 3D space we render a 2D triangle with each vertex having a z position of 0.0, this way the depth remains the same, emulating the 2D effect
+ */
+
+/*  Normalized Device Coordinates (NDC):
+ *  Once your coordinates have been processed in the vertex shader, they should be in NDC which is a small space where the x, y and z values vary from -1.0 to 1.0
+ *  Any coordinates that fall outside this range will be discarded and won't be visible on your screen
+ *  Unlike normal screen coordinate systems, the positive y-axis points in the up-direction and (0,0) is located in the middle of the screen, rather than top left
+ *  Eventually you will want all coordinates to end up in this coordinate space, otherwise they won't be visible
+ *  Your NDC coordinates will then be transformed to screen-space coordinates via the viewpoint transform using the data you provided with GL.Viewport
+ *  The resulting screen-space coordinates are then transformed to fragments as inputs to your fragment shader
+ * 
+ */
+
 public class Window  : GameWindow
 {
+    private float[] vertices =
+    {
+        -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+        0.5f, -0.5f, 0.0f, //Bottom-right vertex
+        0.0f, 0.5f, 0.0f //Top vertex
+    };
+    
     public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
     {
     }
